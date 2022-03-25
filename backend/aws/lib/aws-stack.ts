e7b2377016ed182b6367a05420102c8d0a2d17bc
calebvarghese <calebvar8@gmail.com>
2022-03-24 21:19:50 -0400

    creates aws cdk

---
+++ backend/aws/lib/aws-stack.ts
@@ -0,0 +1,21 @@
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+const env  = { account: '706031659622', region: 'eu-west-1' };
+
+export class resumeStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'AwsQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
+
+const app = new App();
+new resumeStack(app, "resumeStack",{ env: env });
