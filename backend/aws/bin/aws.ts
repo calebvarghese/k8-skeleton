bd30657b119db15c48b9780a70321332f864bc60
calebvarghese <calebvar8@gmail.com>
2022-03-24 22:53:36 -0400

    fixes?

--- backend/aws/bin/aws.ts
+++ backend/aws/bin/aws.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { resumeStack } from '../lib/aws-stack';
+import { AwsStack } from '../lib/aws-stack';
 
 const app = new cdk.App();
-new resumeStack(app, 'AwsStack', {
+new AwsStack(app, 'AwsStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- backend/aws/lib/aws-stack.ts
+++ backend/aws/lib/aws-stack.ts
@@ -1,41 +1,39 @@
-import { CfnOutput, App, Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
+import { CfnOutput, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam'
+import * as s3 from 'aws-cdk-lib/aws-s3'
+import * as s3Deployment from 'aws-cdk-lib/aws-s3-deployment'
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront'
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins'
-import * as iam from 'aws-cdk-lib/aws-iam'
 
-const env  = { account: '706031659622', region: 'eu-west-1' };
-
-export class resumeStack extends Stack {
+export class AwsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+
+    // create bucket and cloud formation for portal SPA hosting
     const siteBucket = new s3.Bucket(this, 'site-bucket',{
-      websiteIndexDocument: 'index.html',
-      websiteErrorDocument: 'index.html',
-      removalPolicy: RemovalPolicy.DESTROY,
+        websiteIndexDocument: 'index.html',
+        websiteErrorDocument: 'index.html',
+        removalPolicy: RemovalPolicy.DESTROY,
     })
     const siteDistOAI = new cloudfront.OriginAccessIdentity(this, `${props?.stackName}-site-dist-oai`);
     const siteDistribution =new cloudfront.Distribution(this, `${props?.stackName}-site-dist`, {
       defaultBehavior: { 
                         origin: new origins.S3Origin(siteBucket), 
                         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
-      },
+                      },
       defaultRootObject: 'index.html',
+      
     });
     siteBucket.addToResourcePolicy(new iam.PolicyStatement({
       actions: ['s3:GetObject'],
       resources: [siteBucket.arnForObjects('*')],
       principals: [new iam.CanonicalUserPrincipal(siteDistOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
     }));
-
+    
     new CfnOutput(this, 'siteBucket',{value: siteBucket.bucketName})
     new CfnOutput(this, 'siteDistribution',{value: siteDistribution.distributionDomainName})
     new CfnOutput(this, 'siteDistributionID',{value: siteDistribution.distributionId})
-
   }
 }
-
-const app = new App();
-new resumeStack(app, "resumeStack",{ env: env });
