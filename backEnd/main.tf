bd453bc8e4930eb07f0fed6bc1a98fac3cf4edd8
Caleb Varghese <calebvarghese@Calebs-MacBook-Pro.local>
2022-07-12 12:00:44 -0400

    add creds again

--- backEnd/main.tf
+++ backEnd/main.tf
@@ -9,7 +9,8 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  alias  = "main"
+  access_key = "${var.AWS_ACCESS_KEY_ID}"
+  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
 }
 
 resource "aws_s3_bucket" "b" {
