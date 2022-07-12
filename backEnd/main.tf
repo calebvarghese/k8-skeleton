ec25fba21a95c4fd76629cb3fd769e93d485bc0e
Caleb Varghese <calebvarghese@Calebs-MacBook-Pro.local>
2022-07-12 11:37:33 -0400

    aws cli + terraform kiss

--- backEnd/main.tf
+++ backEnd/main.tf
@@ -11,3 +11,41 @@ provider "aws" {
   region = "us-east-1"
   alias  = "main"
 }
+
+resource "aws_s3_bucket" "b" {
+  bucket = "caleb-varghese-resume-website-bucket"
+
+  tags = {
+    Name        = "Resume Bucket Example"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "static-website" {
+  bucket = aws_s3_bucket.b.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+
+}
+
+resource "aws_route53_zone" "primary" {
+  name = "calebvarghese.com"
+}
+
+resource "aws_route53_record" "domain" {
+  name    = "calebvarghese.com"
+  zone_id = aws_route53_zone.primary.id
+  type    = "A"
+  alias {
+    name = aws_s3_bucket_website_configuration.static-website.website_domain
+    # zone_id                = aws_s3_bucket_website_configuration.static-website.hosted_zone_id
+    zone_id                = aws_s3_bucket.b.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
