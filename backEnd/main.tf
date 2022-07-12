6291842d1e4834c4e885bc75c5cdf6a38e43aa85
Caleb Varghese <calebvarghese@Calebs-MacBook-Pro.local>
2022-07-12 12:08:51 -0400

    restart

--- backEnd/main.tf
+++ backEnd/main.tf
@@ -7,10 +7,12 @@ terraform {
   }
 }
 
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
 provider "aws" {
   region = "us-east-1"
-  access_key = "${var.TF_VAR_AWS_ACCESS_KEY_ID}"
-  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
 }
 
 resource "aws_s3_bucket" "b" {
